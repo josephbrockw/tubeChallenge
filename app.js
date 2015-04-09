5d78e8eb8b191e55137383e39b269b271e56d4f8
josephbrockw <joew@theroasterie.com>
2015-04-08 22:29:51 -0500

    Pulling information

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
-function handleAPILoaded() {
-	$('#search-button').attr('disabled', false);
-}
+$('Document').ready(function() {
+	function search() {
+		$.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q=derrickrose&key=AIzaSyBcN2ThZ4YKobSTSIUB9a07Api-WGa_4B0', function(data){
+	    	myData = data.Search;
+	    	display(data.items);
+	    	console.log(data.items);
+		});
+	}
 
-function search() {
-	var q = $('#query').val();
-	var request = gapi.clent.youtube.search.list({
-		q: q,
-		part: 'snippet'
-	});
+	function display(videos) {
+		$.each(videos, function(index, video) {
+			console.log(video.snippet.thumbnails.medium.url);
+			$('#search-list').append('<li><img src="' + video.snippet.thumbnails.medium.url + '" /><br><p>' + video.snippet.title + '</p></li>');
+		});
+	}
 
-	request.execute(function(response) {
-		var str = JSON.stringify(response.result);
-		$('#search-container').html('<pre>' + str + '</pre>');
-	});
-}
+
+	search();
+
+	// function displaySearchResults(videos) {
+	// 	var html = "";
+	// 	$.each(videos, function (index, video) {
+	// 		// append li to ul
+	// 		console.log(video.snippet.thumbnails.medium.url);
+	// 		html = html + "<li><p>" + video.snippet.title +
+	// 			"</p><img src='" +  video.snippet.thumbnails.high.url + "'/></li>" ;
+
+	// 	});
+
+	// function getResults(query) {
+	// 	$.getJSON("https://www.googleapis.com/youtube/v3/search", {
+	// 		"part": "snippet",
+	// 		"key": "AIzaSyBcN2ThZ4YKobSTSIUB9a07Api-WGa_4B0",
+	// 		"q": query
+	// 	});
+	// }
+
+	// $('#search-button').submit(function(event) {
+	// 	event.preventDefault();
+	// 	console.log(getResults('Joe'));
+	// })
+	
+});
